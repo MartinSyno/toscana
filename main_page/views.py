51e3df3d9938680058ddeae03f0799724a32efa7
MartinSyno <mrmartin200113@gmail.com>
2021-05-07 18:22:07 +0300

    v3 (оформив нормально html файли)

--- main_page/views.py
+++ main_page/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def main_page_view(request):
-    return render(request, "index.html", context={})
+    return render(request, "main_page_index.html", context={})
