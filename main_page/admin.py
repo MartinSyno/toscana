c58bca2ede0038d0e0c78b1bf5ffe41f7c8c74de
MartinSyno <mrmartin200113@gmail.com>
2021-05-17 14:47:02 +0300

    v17 (підготовка до заливки)

--- main_page/admin.py
+++ main_page/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-@admin.register(SiteSettings)
-class SiteSettingsAdmin(admin.ModelAdmin):
-    list_display = ["address", "phone", "email", "latest_product_amount_in_column"]
-    list_editable = ["phone", "email", "latest_product_amount_in_column"]
-    save_on_top = True
-    list_per_page = 20
+# @admin.register(SiteSettings)
+# class SiteSettingsAdmin(admin.ModelAdmin):
+#     list_display = ["address", "phone", "email", "latest_product_amount_in_column"]
+#     list_editable = ["phone", "email", "latest_product_amount_in_column"]
+#     save_on_top = True
+#     list_per_page = 20
 
 @admin.register(Banner)
 class BannerAdmin(admin.ModelAdmin):
