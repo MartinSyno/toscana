ba4b40b865b1221bc031f162d890e91b304e1493
MartinSyno <mrmartin200113@gmail.com>
2021-05-17 14:42:59 +0300

    v17 (підготовка до заливки)

--- main_page/models.py
+++ main_page/models.py
@@ -2,21 +2,21 @@ from django.db import models
 # Create your models here.
 
 
-class SiteSettings(models.Model):
-    # def save(self, *args, **kwargs):  # Чтобы запрещало создавать более 1 экземпляра этой модели.
-    #     if not self.pk and SiteSettings.objects.exists():
-    #         raise FileExistsError('Не можна створювати два файли налаштувань. Видаліть попередній спочатку.')
-    #     else:
-    #         return super(SiteSettings, self).save(*args, **kwargs)
-    pass
-    # phone = models.CharField(max_length=20)
-    # address = models.CharField(max_length=30)
-    # email = models.EmailField()
-    # facebook_link = models.CharField(max_length=100)
-    # instagram_link = models.CharField(max_length=100)
-    # working_hours = models.CharField(max_length=30)
-    # currency = models.CharField(max_length=10)
-    # latest_product_amount_in_column = models.IntegerField()
+# class SiteSettings(models.Model):
+#     # def save(self, *args, **kwargs):  # Чтобы запрещало создавать более 1 экземпляра этой модели.
+#     #     if not self.pk and SiteSettings.objects.exists():
+#     #         raise FileExistsError('Не можна створювати два файли налаштувань. Видаліть попередній спочатку.')
+#     #     else:
+#     #         return super(SiteSettings, self).save(*args, **kwargs)
+#     pass
+#     # phone = models.CharField(max_length=20)
+#     # address = models.CharField(max_length=30)
+#     # email = models.EmailField()
+#     # facebook_link = models.CharField(max_length=100)
+#     # instagram_link = models.CharField(max_length=100)
+#     # working_hours = models.CharField(max_length=30)
+#     # currency = models.CharField(max_length=10)
+#     # latest_product_amount_in_column = models.IntegerField()
 
 
 class Banner(models.Model):
